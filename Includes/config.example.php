1ed89f30295eea9809d0c7dbab10894df49ef68a
moimenta84 <ikerastra@hotmail.com>
2025-04-18 00:20:30 +0200

    Congig example

--- Includes/config.example.php
+++ Includes/config.example.php
@@ -2,10 +2,9 @@
 // Includes/config.example.php
 // ⚠️ Cópialo como config.php y pon tus propios datos
 
-return [
-    'host' => 'localhost',           // o IP del servidor común
-    'dbname' => 'nombre_de_tu_bd',
-    'username' => 'tu_usuario',
-    'password' => 'tu_contraseña'
-];
+$host = 'localhost';
+$db   = 'Agencia';
+$user = 'tu nombre';     
+$pass = 'tu contraseña'; 
+$port = 'puerto';      
 
